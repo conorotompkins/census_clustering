4cd1f7bf5e678acbdf4a7a4271f5e52b5d090f5b
Conor <conorotompkins@gmail.com>
2020-01-19 18:09:06 -0500

    wrote script to read in study data

---
+++ script/read_study_clusters.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(sf)
+
+
+#data from https://www.openicpsr.org/openicpsr/project/100235/version/V5/view
+
+df <- st_read("data/study/Output-Data_2/US_tract_clusters_new.shp") %>% 
+  select(Geo_NAME, cluster)
+
+df
+
+df %>% 
+  count(cluster, sort = TRUE)
+  
+
+df %>% 
+  filter(str_detect(Geo_NAME, "Allegheny County, Pennsylvania$")) %>% 
+  ggplot(aes(fill = cluster)) +
+    geom_sf(show.legend = FALSE, color = NA)
+  
+
+df %>% 
+  ggplot() +
+    geom_sf()
---
+++ script/references.R
@@ -0,0 +1,3 @@
+#references
+
+#https://www.tandfonline.com/doi/pdf/10.1080/00045608.2015.1052335
